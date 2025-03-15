9b8e4656fd66ea5ff57b448727529cfeda91a4a4
Alex <alex_p97@live.com>
2025-03-15 21:07:44 +0000

    Installed cors to allow cross origin requests

--- Server/DigitalGarden/settings.py
+++ Server/DigitalGarden/settings.py
@@ -38,17 +38,36 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', 
+    'corsheaders',
     'garden',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Add CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4321",  # Your Astro dev server
+]
+
+# Optional: Allow all headers and methods
+CORS_ALLOW_ALL_HEADERS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 ROOT_URLCONF = 'DigitalGarden.urls'
