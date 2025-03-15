4e88fb482ff96bd8f1cf6be8ce4abb1fbda3a030
Alex <alex_p97@live.com>
2025-03-15 18:36:10 +0000

    Added logic for handling form submit

--- Client/src/scripts/api.js
+++ Client/src/scripts/api.js
@@ -2,7 +2,7 @@ const API_URL = "http://127.0.0.1:8000/api/plants/";        //Set the API URL
 
 //Perform a GET request to retrieve all plants from the database
 export async function getPlants() {                         
-  const response = await fetch(API_URL);                    //Use fetch to make a GET request to the API URL
+  const response = await fetch(API_URL);                    //Use fetch to recieve data from the API URL (default is GET)
   return response.json();                                   //Return the response as JSON
 }
 
--- Client/src/scripts/formHandler.js
+++ Client/src/scripts/formHandler.js
@@ -1,1 +1,21 @@
-//JS to handle form submissions
+//Handle form submissions
+import { addPlant } from "./api.js";                                    //Import the POST request function from api.js
+
+const form = document.getElementById("plant-form");                     //Get the form element by its ID
+
+form.addEventListener("submit", async (e) => {                          //Add  event listener for form submit event
+  e.preventDefault();                                                   //Prevent the default form submission (Causes page reload)
+
+  const name = document.getElementById("name").value;                   //Get the values from our input fields
+  const growth_stage = document.getElementById("growth_stage").value;
+  const notes = document.getElementById("notes").value;
+
+  const success = await addPlant({ name, growth_stage, notes });        //Call addPlant and pass the form data as an object
+
+  if (success) {                                                        //addPlant returns a bool, use to check if the request was successful   
+    alert("Plant added successfully!");                                 //Alert the user that the plant was added
+    window.location.reload();                                           //Reload the page to update the plant list
+  } else {
+    alert("Failed to add plant. Please try again.");                    //Show alert if the request failed
+  }
+});
