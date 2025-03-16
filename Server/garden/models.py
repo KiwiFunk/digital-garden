779d08bac6530b9fb8edcbacba1c162217c9e680
Alex <alex_p97@live.com>
2025-03-16 13:40:50 +0000

    Renamed scientific_name field to botanical_field

--- Server/garden/models.py
+++ Server/garden/models.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 class Plant(models.Model):                                  # Define our model class named 'Plant'
     name = models.CharField(max_length=50)                  # Name of the plant. CharField is used to store strings
-    scientific_name = models.CharField(                     # Scientific name of the plant (Optional)
+    botanical_name = models.CharField(                      # Botanical name of the plant (Optional)
         max_length=100, 
         default=""
     )     
