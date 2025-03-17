e9fdd9687375f7a73ca6d8576011647db46ec7b6
Alex <alex_p97@live.com>
2025-03-17 21:44:05 +0000

    Added to serializer to read value from frontend

--- Server/garden/serializers.py
+++ Server/garden/serializers.py
@@ -3,6 +3,7 @@ from .models import Plant
 
 class PlantSerializer(serializers.ModelSerializer):
     needs_watering = serializers.BooleanField(read_only=True)       # Add the boolean field needs_watering to the serializer
+    water_level = serializers.IntegerField(source='get_water_level', read_only=True)
 
     class Meta:
         model = Plant                                               # Specify the model to serialize
