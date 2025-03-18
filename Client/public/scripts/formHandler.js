b0ede3b18952029053bc11756f3cdf591d76943f
Alex <alex_p97@live.com>
2025-03-18 18:58:23 +0000

    Added frontend logic for adding harvest month data

--- Client/public/scripts/formHandler.js
+++ Client/public/scripts/formHandler.js
@@ -11,12 +11,20 @@ form.addEventListener("submit", async (e) => {                          //Add  e
   const watering_frequency = document.getElementById("watering_frequency").value;
   const notes = document.getElementById("notes").value;
 
+  // Get selected months
+  const selectedMonths = Array.from(document.querySelectorAll('.month-checkbox:checked'))
+  .map(checkbox => checkbox.value)
+  .join(',');
+
   const success = await addPlant({                                      //Call the addPlant function with the form data    
     name, 
     botanical_name, 
-    notes, 
+    notes,
+    harvest_months: selectedMonths, 
     watering_frequency 
   });        
+
+  console.log('Final form data:', plantData);                          //Log the form data to the console
 
   if (success) {                                                        //addPlant returns a bool, use to check if the request was successful   
     alert("Plant added successfully!");                                 //Alert the user that the plant was added
