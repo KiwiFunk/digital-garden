1376145deb44559de0c4cc75003c34a40cebf369
Alex <alex_p97@live.com>
2025-03-18 14:25:29 +0000

    Added new endpoint to URL

--- Server/garden/urls.py
+++ Server/garden/urls.py
@@ -4,4 +4,5 @@ from .views import PlantList, PlantDetail
 urlpatterns = [
     path('plants/', PlantList.as_view(), name='plant-list'),                # List and create plants
     path('plants/<int:pk>/', PlantDetail.as_view(), name='plant-detail'),   # Retrieve, update, delete a plant providing its primary key
+    path('plants/<int:pk>/water-level/', PlantDetail.as_view({'get': 'get_water_level'})), #perform a GET request to get the water level of a plant
 ]
